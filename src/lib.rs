ec1e4eba7a95cd41258161efe89592d7bf5b95e4
29 <791603901@qq.com>
2024-09-23 18:32:28 +0800

    🍻 Push temporary code (for testing `crates.io` and `docs.rs`)
    推送临时代码（用于测试`crates.io`和`docs.rs`）

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod ast {
+    pub type AnyRes<T = ()> = Result<T, anyhow::Error>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[cfg(feature = "kernel")]
+pub mod kernel;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use opp::ast::AnyRes;
+use tracing_subscriber::fmt::{format::FmtSpan, time::ChronoLocal};
+
+#[cfg(debug_assertions)]
+fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
+        .with_max_level(tracing::Level::DEBUG)
+        .with_span_events(FmtSpan::FULL)
+        .with_thread_names(true)
+        .init();
+}
+
+#[cfg(not(debug_assertions))]
+fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
+        .with_span_events(FmtSpan::FULL)
+        .with_thread_names(true)
+        .init();
+}
+
+// See: https://github.com/tokio-rs/tracing/issues/3068
+fn init_tracing_with_ansi() {
+    #[cfg(windows)]
+    nu_ansi_term::enable_ansi_support().ok();
+    init_tracing();
+}
+
+#[tokio::main]
+async fn main() -> AnyRes {
+    init_tracing_with_ansi();
+    tracing::info!("Hello, world!");
+    Ok(())
 }

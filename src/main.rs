0f52f451032597e2c99b5a1d356d969f83bc5f6e
29 <791603901@qq.com>
2024-09-28 00:19:34 +0800

    ♻️ (opk): Refactoring the logic of initializing tracing `Subscriber` to prepare for combining multiple `Layer`s
    重构初始化tracing `Subscriber`的逻辑，为组合多`Layer`做准备

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,42 @@
 use opp::ast::AnyRes;
-use tracing_subscriber::fmt::{format::FmtSpan, time::ChronoLocal};
+
+use tracing_subscriber::{
+    filter::LevelFilter,
+    fmt::{self, format::FmtSpan, time::ChronoLocal},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+    Layer,
+};
 
 #[cfg(debug_assertions)]
-fn init_tracing() {
-    tracing_subscriber::fmt()
-        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
-        .with_max_level(tracing::Level::DEBUG)
-        .with_span_events(FmtSpan::FULL)
-        .with_thread_names(true)
-        .init();
+const fn get_level_filter() -> LevelFilter {
+    LevelFilter::DEBUG
 }
 
 #[cfg(not(debug_assertions))]
-fn init_tracing() {
-    tracing_subscriber::fmt()
-        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
-        .with_span_events(FmtSpan::FULL)
-        .with_thread_names(true)
-        .init();
+const fn get_level_filter() -> LevelFilter {
+    LevelFilter::INFO
 }
 
-// See: https://github.com/tokio-rs/tracing/issues/3068
-fn init_tracing_with_ansi() {
+fn init_tracing() {
+    // See: https://github.com/tokio-rs/tracing/issues/3068
     #[cfg(windows)]
     nu_ansi_term::enable_ansi_support().ok();
-    init_tracing();
+
+    tracing_subscriber::registry()
+        .with(
+            fmt::layer()
+                .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
+                .with_span_events(FmtSpan::FULL)
+                .with_thread_names(true)
+                .with_filter(get_level_filter()),
+        )
+        .init();
 }
 
 #[tokio::main]
 async fn main() -> AnyRes {
-    init_tracing_with_ansi();
+    init_tracing();
     tracing::info!("Hello, world!");
     Ok(())
 }
